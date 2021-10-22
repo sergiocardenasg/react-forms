b19847acc7c2fe8fdbe19e6391586c28079d6c59
sergiocardenasg <sergiocardenas@utexas.edu>
2021-10-21 22:19:32 -0500

    Revert "changes"
    This reverts commit 241bd0ca999905214bfe3383ff062f34e179d53c.

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,17 +20,9 @@ class Form extends React.Component {
 
   render() {
     return (
-      <form onSubmit={event => this.handleSubmit(event)}>
-        <input
-          type="text"
-          onChange={event => this.handleFirstNameChange(event)}
-          value={this.state.firstName}
-        />
-        <input
-          type="text"
-          onChange={event => this.handleLastNameChange(event)}
-          value={this.state.lastName}
-        />
+      <form>
+        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
+        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
       </form>
     )
   }
